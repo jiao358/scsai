ca27740ca1f78920d699260e7a69c517ecf447cf
PINO J LUO <jiao358@qq.com>
2017-04-01 23:29:21 +0800

    request.getRequestDispatcher

--- src/main/java/scsai/cmb/control/HelloWorkOne.java
+++ src/main/java/scsai/cmb/control/HelloWorkOne.java
@@ -25,7 +25,8 @@ public class HelloWorkOne {
 	public void error(HttpServletRequest request,HttpServletResponse response) throws Exception{
 		
 		 logger.error("Test error 500 page");
-		 response.sendRedirect("scsai/static/500.html");
+//		 response.sendRedirect("scsai/static/500.html");
+		 request.getRequestDispatcher("/static/500.html").forward(request,response);
 	}
 	 @RequestMapping(value="/error2.do",method=RequestMethod.GET)
 		public void error2(HttpServletRequest request,HttpServletResponse response) throws Exception{
