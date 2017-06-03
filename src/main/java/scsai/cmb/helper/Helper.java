d0175327b51f1dd24e337f7f826b3ff4d7618da6
PINO J LUO <jiao358@qq.com>
2017-06-03 16:58:26 +0800

    add ip

--- src/main/java/scsai/cmb/helper/Helper.java
+++ src/main/java/scsai/cmb/helper/Helper.java
@@ -2,6 +2,11 @@ package scsai.cmb.helper;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +20,38 @@ import scsai.cmb.control.KillSystem;
 public class Helper {
 	private static final String ENCODING="UTF-8";
 	private static final Logger logger = Logger.getLogger(Helper.class);
+	private static String IP=null;
+	
+	
+	private static void ipinit(){
+		Enumeration allNetInterfaces;
+		try {
+			allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+			InetAddress ip = null;
+			while (allNetInterfaces.hasMoreElements())
+			{
+			NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+			if(!netInterface.getName().equals("eth0")){
+				continue;
+			}
+			Enumeration addresses = netInterface.getInetAddresses();
+			while (addresses.hasMoreElements())
+			{
+			ip = (InetAddress) addresses.nextElement();
+			if (ip != null && ip instanceof Inet4Address)
+			{
+				IP=ip.getHostAddress();
+			} 
+			}
+			}	
+		} catch (SocketException e) {
+			logger.error("IP initError!");
+		}
+		
+	}
+	
+	
+	
 	public static void restful(HttpServletResponse  response , Object bean) throws IOException{
 		ObjectMapper mapper = new ObjectMapper();
 		response.setHeader("Context-Type", "application/json;charset=UTF-8");
@@ -29,16 +66,22 @@ public class Helper {
 	}
 	
 	public static Map initResponse(){
+		if(IP==null)
+			ipinit();
 		Map<String,String> map = new HashMap<String,String>();
 		map.put("state", "1");
 		map.put("code", "");
 		map.put("message", "");
+		map.put("ip", IP);
 		return map;
 	}
 	public static void processError(HttpServletResponse  response){
+		if(IP==null)
+			ipinit();
 		Map<String,String> map = new HashMap<String,String>();
 		map.put("state", "2");
 		map.put("code", "");
 		map.put("message", "sometings error");
+		map.put("ip", IP);
 	}
 }
